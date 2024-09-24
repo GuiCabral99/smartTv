2301566daa5d317b7e052397959e9cdb3b082779
Guilherme Cabral <gui.cabral7641@gmail.com>
2024-09-24 01:54:30 -0300

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,3 +1,9 @@
+/**
+ * <h1>User</h1>
+ * <p>This class simulates an user using a TV</p>
+ * <p>Here we can use methodos that are often used on a TV</p>
+*/
+
 public class User {
     public static void main(String[] args) {
         SmartTv smartTv = new SmartTv();
